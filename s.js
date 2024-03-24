02a72f12c713faacfd2b329675ff12e130e1602e
stephen354 <stephenjokam354@gmail.com>
2024-03-25 05:48:00 +0700

    Sudah

--- s.js
+++ s.js
@@ -1,5 +1,15 @@
 //----------Hari-------------
 
+  var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+  var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
+
+  let time = new Date();
+  let minutes = time.getMinutes() > 9 ? time.getMinutes() : "0"+time.getMinutes();
+  let tanggal = myDays[time.getDay()] + ", " + time.getDate() + " " + months[time.getMonth()] ;
+
+  document.getElementById('time').innerHTML = time.getHours() + ":" + minutes;
+  document.getElementById('hari').innerHTML = tanggal;
+
 
 //------------ audio -------------
 
@@ -31,43 +41,37 @@ var i = 1;
 
 // const catatan2 = document.querySelector("#note2");
 function notes(){
-  
+
     document.querySelector(".btnPesan").style.display = "none";
-    if(i==1){
-      animasi.style.animation = "fade2 1s" ;
-      
-    catatan.innerHTML = catatan2[i-1];
-    
     document.getElementById("balas").style.display = "none";
     document.getElementById("balas2").style.display = "none";
     document.getElementById("hr").style.display = "none";
-  // catatan.style.display = "none";
-  // catatan2.style.display = "flex";
-    
-     i++;
-    }else if(i==2){
-      animasi.style.animation = "fade 1s" ;
-       catatan.innerHTML = catatan2[i-1]; 
-       setTimeout(display,2000);
-        i++;
-        
-    }else if(i==3){
-      animasi.style.animation = "fade2 1s" ;
-        catatan.innerHTML= catatan2[i-1];
-        setTimeout(display,4000);
-        i++;
-    }else if(i==4){
+    switch(i){
+      case 1 : 
+      case 2 : 
+      case 3 : 
+        setTimeout(display,3000);
         animasi.style.animation = "fade 1s" ;
         catatan.innerHTML= catatan2[i-1]
-        setTimeout(displayKado,2000);
         i++;
-    }else if(i==5){
+        break;
+      case 4 :
         kado.style.display = "none";
         catatan.innerHTML= catatan2[i-1]
-        setTimeout(display,4000);
+        setTimeout(displayKado,4000);
         i++;
+        break;
+      case 5 :
+      case 6 :
+        kado.style.display = "none";
+        catatan.innerHTML= catatan2[i-1]
+        setTimeout(display,3000);
+        i++; 
+        break;
+      default: 
+      catatan.innerHTML= "See yaaa <br> Kirim Pesan😊";
+      document.querySelector(".kirimPesan").style.display = "flex";
     }
-
 }
 
 // ---------------- Button -------------
@@ -75,6 +79,7 @@ function notes(){
       const btnPesan = document.querySelector(".btnPesan");
       const emotlucu = document.querySelector(".emotlucu");
       const kado = document.querySelector(".img_kado");
+      
 
       function displayKado(){
         kado.style.display = "flex";
